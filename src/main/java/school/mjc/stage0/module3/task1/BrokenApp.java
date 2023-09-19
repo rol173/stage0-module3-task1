b1cba6464f128e07d4c548e139a441a312ab136b
Alexander Beisel <golivud@gmail.com>
2023-09-19 12:24:33 +0200

    1

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.module3.task1;
 
 public class BrokenApp {
     public static void main(String[] args) {
-        System.out.println("fix"+"\nMe");
+        System.out.println("""
+                fix
+                Me""");
     }
 }
